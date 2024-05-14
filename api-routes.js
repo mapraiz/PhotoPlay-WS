95772e3e707f8943635a294786eaab16ff36c4ae
LILIANADAM1 <MMORENO.LILIANA@GMAIL.COM>
2024-05-14 09:15:05 +0200

    index

---
+++ api-routes.js
@@ -0,0 +1,51 @@
+//Filename api-routes.js
+
+// Intialize express router
+let router = require('express').Router();
+
+
+// Set default API response
+router.get('/', function(req, res)
+{
+    res.json(
+    {
+        status: 'API Trabajando',
+        message: 'Bienvenido al mejor WS del mundo'
+    });
+});
+
+const { get } = require('mongoose');
+
+//import Content controller
+var playerController = require('./playerController');
+var scoreController = require('./scoreController');
+
+
+
+//routes
+router.route('/player')
+.get(playerController.index)
+.post(playerController.new)
+
+router.route('/player/:player_name')
+.delete(playerController.delete)
+.put(playerController.update)
+.get(playerController.indexByName)
+
+router.route('/login')
+.post(playerController.login)
+
+router.route('/score/top')
+.get(playerController.indexTop10)
+
+router.route('/score')
+.get(scoreController.index)
+.post(scoreController.new)
+
+router.route('/delete/:player_id')
+.delete(playerController.deletePlayer)
+
+
+
+//export 
+module.exports = router;
--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-//prueba1
+// Filename: index.js
+
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path'); // Import path module for file paths
+
+console.log('Running AFG web service index');
+
+// Import Router
+const apiRoutes = require("./api-routes");
+
+// Initialize the app
+const app = express();
+
+// Enable CORS
+app.use(cors());
+
+// Configure body-parser to handle post requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/afg', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+if (!db) {
+    console.log("Error connecting to db");
+} else {
+    console.log("DB connected successfully");
+}
+
+// Setup server port
+const port = process.env.PORT || 8080;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define route for serving index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Launch app to listen to specified port
+app.listen(port, () => {
+    console.log(`Running RestHub for AFG on port ${port}`);
+});
+
+// Define API routes
+app.use('/api', apiRoutes);
