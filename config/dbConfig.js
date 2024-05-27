6fd929f4258557f14249fc3aef52decec34b856a
diegoreto7u7 <diegoret2005@gmail.com>
2024-05-27 14:04:35 +0200

    adsad

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const oracledb = require('oracledb');
 
 async function initialize() {
     await oracledb.createPool({
-        user: 'photoplay',
+        user: 'c##photoplay',
         password: 'almi123',
         connectString: '3.221.255.12:1521/ORCLCDB'
     });
