0526995fc38e2e27b1e203b3694432ec7bef2956
diegoreto7u7 <diegoret2005@gmail.com>
2024-05-27 12:17:45 +0200

    sdfdsf

---
+++ controllers/javaController.js
@@ -0,0 +1,64 @@
+const oracledb = require('oracledb');
+const dbConfig = require('../config/dbConfig');
+
+// Inicializar la conexión a la base de datos
+async function initialize() {
+    await dbConfig.initialize();
+}
+
+initialize();
+
+async function validarUsuario(username, contrasena) {
+    let connection;
+    let isValid = false;
+    try {
+        connection = await oracledb.getConnection();
+        const bindVars = {
+            p_username: { dir: oracledb.BIND_IN, val: username },
+            p_password: { dir: oracledb.BIND_IN, val: contrasena },
+            p_valid: { dir: oracledb.BIND_OUT, type: oracledb.NUMBER }
+        };
+        const result = await connection.execute(
+            `BEGIN
+                 VALIDAR_USUARIO(:p_username, :p_password, :p_valid);
+             END;`,
+            bindVars,
+            { autoCommit: true }
+        );
+
+        isValid = result.outBinds.p_valid === 1;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    } finally {
+        if (connection) {
+            try {
+                await connection.close();
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+    return isValid;
+}
+
+async function comprobarUsuario(req, res) {
+    const { username, contrasena } = req.query;
+
+    try {
+        const usuarioValido = await validarUsuario(username, contrasena);
+
+        if (usuarioValido) {
+            res.status(200).send("Usuario válido");
+        } else {
+            res.status(401).send("Usuario inválido");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error interno");
+    }
+}
+
+module.exports = {
+    comprobarUsuario
+};
